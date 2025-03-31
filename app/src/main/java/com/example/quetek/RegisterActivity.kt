e1c4a4f0fe3cd56efeebdc7469881ae1d891d9a8
Jamjamzz <codejamzz27@gmail.com>
2025-03-31 22:21:07 +0800

    firebase

--- app/src/main/java/com/example/quetek/RegisterActivity.kt
+++ app/src/main/java/com/example/quetek/RegisterActivity.kt
@@ -1,23 +1,26 @@
 package com.example.quetek
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.FirebaseDatabase
 
-
-class RegisterActivity : Activity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        FirebaseApp.initializeApp(this)
 
         val etIdNumber = findViewById<EditText>(R.id.etIdNumber)
         val etFirstName = findViewById<EditText>(R.id.etFirstName)
-        val etMiddleInitial = findViewById<EditText>(R.id.etMiddleInitial)
         val etLastName = findViewById<EditText>(R.id.etLastName)
+        val etProgram = findViewById<EditText>(R.id.etProgram)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val etConfirmPassword = findViewById<EditText>(R.id.etConfirmPassword)
 
         val btnSubmit: Button = findViewById(R.id.btnSubmit)
         val btnCancel: Button = findViewById(R.id.btnCancel)
@@ -27,7 +30,26 @@ class RegisterActivity : Activity() {
         }
 
         btnSubmit.setOnClickListener {
-            startActivity(Intent(this, LandingActivity::class.java))
+            val database = FirebaseDatabase.getInstance().reference
+
+            val userId = database.child("users").push().key
+
+            if (userId != null) {
+                val user = HashMap<String, String>()
+                user["id"] = etIdNumber.text.toString()
+                user["password"] = etPassword.text.toString()
+                user["username"] = "${etFirstName.text} ${etLastName.text}"
+                user["program"] = etProgram.text.toString()
+
+                database.child("users").child(userId).setValue(user)
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "User added successfully!", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, LandingActivity::class.java))
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(this, "Failed to add user!", Toast.LENGTH_SHORT).show()
+                    }
+            }
         }
     }
 }
